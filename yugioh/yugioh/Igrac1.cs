ae5c8e71465ded893e2b9225df9141df6c5d3bc9
Dimitri Zelkov <Dimitri Zelkov>
2016-05-06 17:28:45 +0200

    Dodavanje na igrac1 klasa

---
+++ yugioh/yugioh/Igrac1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yugioh
+{
+    class Igrac1
+    {
+        int Magic = 0;
+        int Move = 0;
+        int Attack = 0;
+        int Deffend = 0;
+    }
+}
