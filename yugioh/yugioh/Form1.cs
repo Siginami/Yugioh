80310385d6120f5a47903f60807e8e6bbd06e37a
Dimitri Zelkov <Dimitri Zelkov>
2016-05-08 02:56:44 +0200

    Implementirana metoda za izbor na kocka

--- yugioh/yugioh/Form1.cs
+++ yugioh/yugioh/Form1.cs
@@ -21,11 +21,26 @@ namespace yugioh
         protected int currentSize = 10;
         protected bool drawingMode = true;
         protected Point moveClickPosition;
+        Kocki kocki1 = new Kocki("Summon2", "Summon2", "Summon2", "Magic", "Move", "Move");
+        Kocki kocki2 = new Kocki("Summon2", "Summon2", "Summon2", "Move", "Deffend", "Attack");
+        Kocki kocki3 = new Kocki("Summon2", "Summon2", "Summon2", "Attack", "Attack", "Magic");
+        Kocki kocki4 = new Kocki("Summon2", "Summon2", "Summon2", "Deffend", "Attack", "Magic");
+        Kocki kocki5 = new Kocki("Summon2", "Summon2", "Summon2", "Magic", "Magic", "Deffend");
+        Kocki kocki6 = new Kocki("Summon2", "Move", "Attack", "Magic", "Deffend", "Move2");
+        Kocki kocki7 = new Kocki("Summon2", "Move", "Magic", "Magic", "Deffend", "Deffend");
+        Kocki kocki8 = new Kocki("Summon2", "Attack", "Attack", "Magic", "Magic", "Deffend");
+        Kocki kocki9 = new Kocki("Summon2", "Summon2", "Summon2", "Attack", "Move", "Deffend");
+        Kocki kocki10 = new Kocki("Summon3", "Summon3", "Attack2", "Magic", "Move2", "Deffend");
+        Kocki kocki11 = new Kocki("Summon3", "Summon3", "Attack", "Magic", "Move2", "Deffend2");
+        Kocki kocki12 = new Kocki("Summon3", "Summon3", "Attack", "Magic2", "Move2", "Deffend");
+        Kocki kocki13 = new Kocki("Summon3", "Summon3", "Attack", "Magic2", "Move", "Deffend2");
+        Kocki kocki14 = new Kocki("Summon4", "Magic2", "Attack2", "Move2", "Deffend2", "Move3");
+        Kocki kocki15 = new Kocki("Summon4", "Move2", "Magic2", "Deffend2", "Attack2", "Magic3");
         public Form1()
         {
             InitializeComponent();
             docGrForms = new DrawDoc();
-            Kocki kocka1 = new Kocki("Summon2", "Summon2", "Summon2", "Magic", "Move", "Move");
+            /*Kocki kocka1 = new Kocki("Summon2", "Summon2", "Summon2", "Magic", "Move", "Move");
             Kocki kocka2 = new Kocki("Summon2", "Summon2", "Summon2", "Move", "Deffend", "Attack");
             Kocki kocka3 = new Kocki("Summon2", "Summon2", "Summon2", "Attack", "Attack", "Magic");
             Kocki kocka4 = new Kocki("Summon2", "Summon2", "Summon2", "Deffend", "Attack", "Magic");
@@ -39,7 +54,7 @@ namespace yugioh
             Kocki kocka12 = new Kocki("Summon3", "Summon3", "Attack", "Magic2", "Move2", "Deffend");
             Kocki kocka13 = new Kocki("Summon3", "Summon3", "Attack", "Magic2", "Move", "Deffend2");
             Kocki kocka14 = new Kocki("Summon4", "Magic2", "Attack2", "Move2", "Deffend2", "Move3");
-            Kocki kocka15 = new Kocki("Summon4", "Move2", "Magic2", "Deffend2", "Attack2", "Magic3");
+            Kocki kocka15 = new Kocki("Summon4", "Move2", "Magic2", "Deffend2", "Attack2", "Magic3");*/
         }
         private void label1_Click(object sender, EventArgs e)
         {
@@ -98,11 +113,372 @@ namespace yugioh
             }
         }
 
+        private string PresmetajValue(int randomizer, int randomzier2)
+        {
+            string value = "";
+            switch (randomizer)
+            {
+                case 1:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki1.strana1;
+                            break;
+                        case 2:
+                            value = kocki1.strana2;
+                            break;
+                        case 3:
+                            value = kocki1.strana3;
+                            break;
+                        case 4:
+                            value = kocki1.strana4;
+                            break;
+                        case 5:
+                            value = kocki1.strana5;
+                            break;
+                        case 6:
+                            value = kocki1.strana6;
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki2.strana1;
+                            break;
+                        case 2:
+                            value = kocki2.strana2;
+                            break;
+                        case 3:
+                            value = kocki2.strana3;
+                            break;
+                        case 4:
+                            value = kocki2.strana4;
+                            break;
+                        case 5:
+                            value = kocki2.strana5;
+                            break;
+                        case 6:
+                            value = kocki2.strana6;
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki3.strana1;
+                            break;
+                        case 2:
+                            value = kocki3.strana2;
+                            break;
+                        case 3:
+                            value = kocki3.strana3;
+                            break;
+                        case 4:
+                            value = kocki3.strana4;
+                            break;
+                        case 5:
+                            value = kocki3.strana5;
+                            break;
+                        case 6:
+                            value = kocki3.strana6;
+                            break;
+                    }
+                    break;
+                case 4:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki4.strana1;
+                            break;
+                        case 2:
+                            value = kocki4.strana2;
+                            break;
+                        case 3:
+                            value = kocki4.strana3;
+                            break;
+                        case 4:
+                            value = kocki4.strana4;
+                            break;
+                        case 5:
+                            value = kocki4.strana5;
+                            break;
+                        case 6:
+                            value = kocki4.strana6;
+                            break;
+                    }
+                    break;
+                case 5:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki5.strana1;
+                            break;
+                        case 2:
+                            value = kocki5.strana2;
+                            break;
+                        case 3:
+                            value = kocki5.strana3;
+                            break;
+                        case 4:
+                            value = kocki5.strana4;
+                            break;
+                        case 5:
+                            value = kocki5.strana5;
+                            break;
+                        case 6:
+                            value = kocki5.strana6;
+                            break;
+                    }
+                    break;
+                case 6:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki6.strana1;
+                            break;
+                        case 2:
+                            value = kocki6.strana2;
+                            break;
+                        case 3:
+                            value = kocki6.strana3;
+                            break;
+                        case 4:
+                            value = kocki6.strana4;
+                            break;
+                        case 5:
+                            value = kocki6.strana5;
+                            break;
+                        case 6:
+                            value = kocki6.strana6;
+                            break;
+                    }
+                    break;
+                case 7:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki7.strana1;
+                            break;
+                        case 2:
+                            value = kocki7.strana2;
+                            break;
+                        case 3:
+                            value = kocki7.strana3;
+                            break;
+                        case 4:
+                            value = kocki7.strana4;
+                            break;
+                        case 5:
+                            value = kocki7.strana5;
+                            break;
+                        case 6:
+                            value = kocki7.strana6;
+                            break;
+                    }
+                    break;
+                case 8:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki8.strana1;
+                            break;
+                        case 2:
+                            value = kocki8.strana2;
+                            break;
+                        case 3:
+                            value = kocki8.strana3;
+                            break;
+                        case 4:
+                            value = kocki8.strana4;
+                            break;
+                        case 5:
+                            value = kocki8.strana5;
+                            break;
+                        case 6:
+                            value = kocki8.strana6;
+                            break;
+                    }
+                    break;
+                case 9:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki9.strana1;
+                            break;
+                        case 2:
+                            value = kocki9.strana2;
+                            break;
+                        case 3:
+                            value = kocki9.strana3;
+                            break;
+                        case 4:
+                            value = kocki9.strana4;
+                            break;
+                        case 5:
+                            value = kocki9.strana5;
+                            break;
+                        case 6:
+                            value = kocki9.strana6;
+                            break;
+                    }
+                    break;
+                case 10:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki10.strana1;
+                            break;
+                        case 2:
+                            value = kocki10.strana2;
+                            break;
+                        case 3:
+                            value = kocki10.strana3;
+                            break;
+                        case 4:
+                            value = kocki10.strana4;
+                            break;
+                        case 5:
+                            value = kocki10.strana5;
+                            break;
+                        case 6:
+                            value = kocki10.strana6;
+                            break;
+                    }
+                    break;
+                case 11:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki11.strana1;
+                            break;
+                        case 2:
+                            value = kocki11.strana2;
+                            break;
+                        case 3:
+                            value = kocki11.strana3;
+                            break;
+                        case 4:
+                            value = kocki11.strana4;
+                            break;
+                        case 5:
+                            value = kocki11.strana5;
+                            break;
+                        case 6:
+                            value = kocki11.strana6;
+                            break;
+                    }
+                    break;
+                case 12:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki12.strana1;
+                            break;
+                        case 2:
+                            value = kocki12.strana2;
+                            break;
+                        case 3:
+                            value = kocki12.strana3;
+                            break;
+                        case 4:
+                            value = kocki12.strana4;
+                            break;
+                        case 5:
+                            value = kocki12.strana5;
+                            break;
+                        case 6:
+                            value = kocki12.strana6;
+                            break;
+                    }
+                    break;
+                case 13:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki13.strana1;
+                            break;
+                        case 2:
+                            value = kocki13.strana2;
+                            break;
+                        case 3:
+                            value = kocki13.strana3;
+                            break;
+                        case 4:
+                            value = kocki13.strana4;
+                            break;
+                        case 5:
+                            value = kocki13.strana5;
+                            break;
+                        case 6:
+                            value = kocki13.strana6;
+                            break;
+                    }
+                    break;
+                case 14:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki14.strana1;
+                            break;
+                        case 2:
+                            value = kocki14.strana2;
+                            break;
+                        case 3:
+                            value = kocki14.strana3;
+                            break;
+                        case 4:
+                            value = kocki14.strana4;
+                            break;
+                        case 5:
+                            value = kocki14.strana5;
+                            break;
+                        case 6:
+                            value = kocki14.strana6;
+                            break;
+                    }
+                    break;
+                case 15:
+                    switch (randomzier2)
+                    {
+                        case 1:
+                            value = kocki15.strana1;
+                            break;
+                        case 2:
+                            value = kocki15.strana2;
+                            break;
+                        case 3:
+                            value = kocki15.strana3;
+                            break;
+                        case 4:
+                            value = kocki15.strana4;
+                            break;
+                        case 5:
+                            value = kocki15.strana5;
+                            break;
+                        case 6:
+                            value = kocki15.strana6;
+                            break;
+                    }
+                    break;
+            }
+            return value;
+        }
+
         private void endturn_Click(object sender, EventArgs e)
         {
+
             int randomizer = brojce.Next(1, 15);
-            int randomizer2 = brojce.Next(1, 6);
-            //Kako da pristapam do kockata so broj randomizer i stranata so broj randomizer2???
+            int randomzier2 = brojce.Next(1, 6);
+            int randomizer3 = brojce.Next(1, 15);
+            int randomzier4 = brojce.Next(1, 6);
+            int randomizer5= brojce.Next(1, 15);
+            int randomzier6 = brojce.Next(1, 6);
+            string prvakocka = PresmetajValue(randomizer, randomzier2);
+            string vtorakocka = PresmetajValue(randomizer3, randomzier4);
+            string tretakocka = PresmetajValue(randomizer5,randomzier6);
         }
     }
 }
